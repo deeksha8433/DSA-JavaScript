a90bf454133abecd6cbc8fe7bd64044712c2d3c7
Deeksha <your-email@gmail.com>
2026-04-09 18:08:32 +0530

    Day 1: Two Sum problem

---
+++ two-sum.js
@@ -0,0 +1,18 @@
+// Problem: Two Sum
+// Day 1
+
+function twoSum(nums, target) {
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+
+    map[nums[i]] = i;
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
